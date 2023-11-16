59d2153b6294cbbf12c2588539b8e86224aaa23d
Sidowe <jaderson6100@gmail.com>
2023-11-16 16:55:31 -0400

    Criando as Classes DAO

--- 4 - Cods/JAVA/1 - JAVAFX/projetoJavaFX/src/dao/CarroDAO.java
+++ 4 - Cods/JAVA/1 - JAVAFX/projetoJavaFX/src/dao/CarroDAO.java
@@ -1,24 +1,28 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import objetos.Carro;
-import objetos.Usuario;
 
 public class CarroDAO {
+
     private final Connection connection;
 
     public CarroDAO(Connection connection) {
         this.connection = connection;
-    }    
-    /*public Usuario obterCarroPorId(int userId) {
-        String query = "SELECT * FROM usuario WHERE userId = ?";
+    }
+
+    public Carro obterCarroPorId(int carroId) {
+        String query = "SELECT * FROM usuario WHERE carroId = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(1, carroId);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
-                    return criarUsuario(resultSet);
+                    return criarCarro(resultSet);
                 }
             }
         } catch (SQLException e) {
@@ -26,13 +30,59 @@ public class CarroDAO {
         }
         return null;
     }
+
+    public List<Carro> obterTodosCarros() {
+        List<Carro> carros;
+        carros = new ArrayList<>();
+        String query = "SELECT * FROM carro";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query); ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                carros.add(criarCarro(resultSet));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return carros;
+    }
+
+    public void salvarCarro(Carro carro) {
+        String query = "INSERT INTO carro (nomeCarro, DescricaoCarro) VALUES (?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, carro.getNomeCarro());
+            preparedStatement.setString(2, carro.getDescricaoCarro());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void atualizarCarro(Carro carro) {
+        String query = "UPDATE carro SET nomeCarro = ?, DescricaoCarro = ? WHERE carroId = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, carro.getNomeCarro());
+            preparedStatement.setString(2, carro.getDescricaoCarro());
+            preparedStatement.setInt(3, carro.getCarroId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void excluirCarro(int carroId) {
+        String query = "DELETE FROM carro WHERE carroId = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, carroId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
     private Carro criarCarro(ResultSet resultSet) throws SQLException {
-        Carro carro = new Usuario();
-        carro.setNome(resultSet.getString("nome"));
-        carro.setEmail(resultSet.getString("email"));
-        carro.setSenha(resultSet.getString("senha"));
-        carro.setAdministrador(resultSet.getBoolean("administrador"));
-        return usuario;
-    }**/
-   
+        Carro carro = new Carro();
+        carro.setNomeCarro(resultSet.getString("nomeCarro"));
+        carro.setDescricaoCarro(resultSet.getString("DescricaoCarro"));
+        return carro;
+    }
+
 }
